c966b18356847ca202aa9f91756782689a1b58ec
GwenRaq <gwen.raq@gmail.com>
2016-10-23 21:13:18 -1000

    ok

---
+++ index.js
@@ -0,0 +1,15 @@
+var green = 7000;
+var red = 1000;
+var blue = 2000;
+
+var aliens = green + red + blue;
+
+if (aliens == 10000){
+	console.log("So much!");
+}if (green > 5000){
+	console.log ("Eww!");
+}if (green + red >= 8000){
+	console.log ("Ugh..");
+}if (red && blue < 3000){
+	console.log("This is okay.");
+}
